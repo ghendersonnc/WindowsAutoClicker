e3c0ba346878ceb7ebe0e7834843d17208300b72
ghendersonnc <ghendersonnc@gmail.com>
2021-03-10 04:52:38 -0500

    Changed keys to start autoclick

--- AutoClicker/Clickers.cpp
+++ AutoClicker/Clickers.cpp
@@ -4,38 +4,36 @@
 
 namespace clicks {
 	void autoClick() {
-		// Auto clicks 10 times per second
-		while (GetKeyState(VK_NUMPAD5) & 1) {
+		while (GetKeyState(VK_F8) & 1) {
 
-			// Untoggle NUMPAD6 if pressed while autoClickLock is active
-			if (GetKeyState(VK_NUMPAD6) & 1) {
-				keybd_event(VK_NUMPAD6, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
-				keybd_event(VK_NUMPAD6, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+			// Untoggle F9 if pressed while autoClick is active
+			if (GetKeyState(VK_F9) & 1) {
+				keybd_event(VK_F9, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+				keybd_event(VK_F9, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
 			}
 
 			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 			Sleep(50);
 
-			std::cout << "press" << std::endl;
 		}
 	}
+
 	void autoClickLock() {
 		POINT point;
 		GetCursorPos(&point);
 
-		while (GetKeyState(VK_NUMPAD6) & 1) {
+		while (GetKeyState(VK_F9) & 1) {
 
-			// Untoggle NUMPAD5 if pressed while autoClickLock is active
-			if (GetKeyState(VK_NUMPAD5) & 1) {
-				keybd_event(VK_NUMPAD5, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
-				keybd_event(VK_NUMPAD5, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+			// Untoggle F8 if pressed while autoClickLock is active
+			if (GetKeyState(VK_F8) & 1) {
+				keybd_event(VK_F8, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+				keybd_event(VK_F8, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
 			}
 
 			SetCursorPos(point.x, point.y);
 			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 			Sleep(50);
 
-			std::cout << "press" << std::endl;
 		}
 	}
 }
--- AutoClicker/Main.cpp
+++ AutoClicker/Main.cpp
@@ -3,18 +3,22 @@
 #include "Clickers.h"
 
 // Clicks 20 times per second
-using namespace clicks; // From Clickers.h
+using namespace clicks;
 
 int main() {
+	
+	std::cout << "Press F8 to start autoclick" << std::endl;
+	std::cout << "Press F9 to start autoclick (Locks cursor in place" << std::endl;
 	bool running = true;
-	// Press NUMPAD5 to activate auto click
+	// F8 and F9 start autoclick
+
 	while (running) {
-		if (GetKeyState(VK_NUMPAD5) & 1) {
+		if (GetKeyState(VK_F8) & 1) {
 			// Normal auto clicking
 			autoClick();
 		}
 
-		if (GetKeyState(VK_NUMPAD6) & 1) {
+		if (GetKeyState(VK_F9) & 1) {
 			// Locks cursor at current coordinates and auto clicks
 			autoClickLock();
 		}
