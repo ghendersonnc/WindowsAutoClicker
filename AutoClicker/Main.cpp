2b46e1764970841f779b5d12b4c2dba6a1b48850
ghendersonnc <ghendersonnc@gmail.com>
2021-03-10 00:46:50 -0500

    Fixed not auto-clicking while locked in place

--- AutoClicker/Main.cpp
+++ AutoClicker/Main.cpp
@@ -1,46 +1,64 @@
 #include <iostream>
 #include <Windows.h>
 
-void lockCursor(POINT);
-void mouseClick();
+void autoClick();
+void autoClickLock();
 
 int main() {
 	bool running = true;
 	// Press NUMPAD5 to activate auto click
 	while (running) {
 		if (GetKeyState(VK_NUMPAD5) & 1) {
-			mouseClick();
+			autoClick();
+		}
+
+		if (GetKeyState(VK_NUMPAD6) & 1) {
+			autoClickLock();
 		}
 
 		// Quit program, does not work while autoclicking is active
 		if (GetKeyState(VK_ESCAPE) & 0x8000) {
 			running = false;
 		}
+
 	}
 
 	return 0;
 }
 
-// Maintains cursor coordinates
-void lockCursor(POINT point) {
-	while (GetKeyState(VK_NUMPAD0) == 1) {
-		SetCursorPos(point.x, point.y);
+void autoClick() {
+	// Auto clicks 10 times per second
+	while (GetKeyState(VK_NUMPAD5) & 1) {
 
+		// Untoggle NUMPAD6 if pressed while autoClickLocl is active
+		if (GetKeyState(VK_NUMPAD6) & 1) {
+			keybd_event(VK_NUMPAD6, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+			keybd_event(VK_NUMPAD6, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+		}
+
+		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+		Sleep(50);
+			
+		std::cout << "press" << std::endl;
 	}
 }
 
-void mouseClick() {
+void autoClickLock() {
 	POINT point;
-	// Auto clicks 10 times per second
-	while (GetKeyState(VK_NUMPAD5) == 1) {
-		GetCursorPos(&point);
-		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+	GetCursorPos(&point);
+	
+	while (GetKeyState(VK_NUMPAD6) & 1) {
 
-		// Pressing NUMPAD0 locks cursor in place
-		if (GetKeyState(VK_NUMPAD0) & 1) {
-			lockCursor(point);
+		// Untoggle NUMPAD5 if pressed while autoClickLock is active
+		if (GetKeyState(VK_NUMPAD5) & 1) {
+			keybd_event(VK_NUMPAD5, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+			keybd_event(VK_NUMPAD5, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
 		}
+		
+		SetCursorPos(point.x, point.y);
+		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+		Sleep(50);
 
-		Sleep(100);
+		std::cout << "press" << std::endl;
 	}
 }
